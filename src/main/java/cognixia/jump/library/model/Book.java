717142319712c5ac0aab796e9033537860daf890
Kianoosh Mokhtari <kianooshmokhtari@outlook.com>
2021-06-30 17:00:28 -0400

    created model files

--- src/main/java/cognixia/jump/library/model/Book.java
+++ src/main/java/cognixia/jump/library/model/Book.java
@@ -1,9 +1,65 @@
 package cognixia.jump.library.model;
 
+import java.util.Date;
+
 public class Book {
 	
 	private String ISBN;
 	private String title;
+	private String description;
+	private boolean isRented;
+	private Date added_To_Library;
+	
+	public Book(String iSBN, String title, String description, boolean isRented, Date added_To_Library) {
+		super();
+		ISBN = iSBN;
+		this.title = title;
+		this.description = description;
+		this.isRented = isRented;
+		this.added_To_Library = added_To_Library;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isRented() {
+		return isRented;
+	}
+
+	public void setRented(boolean isRented) {
+		this.isRented = isRented;
+	}
+
+	public Date getAdded_To_Library() {
+		return added_To_Library;
+	}
+
+	public void setAdded_To_Library(Date added_To_Library) {
+		this.added_To_Library = added_To_Library;
+	}
+	
+	
 	
 
 }
--- src/main/java/cognixia/jump/library/model/Patron.java
+++ src/main/java/cognixia/jump/library/model/Patron.java
@@ -1,5 +1,57 @@
 package cognixia.jump.library.model;
 
 public class Patron {
+	
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private boolean accountFrozen;
+	public Patron(String id, String firstName, String lastName, String userName, String password
+			) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.accountFrozen = false;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isAccountFrozen() {
+		return accountFrozen;
+	}
+	public void setAccountFrozen(boolean accountFrozen) {
+		this.accountFrozen = accountFrozen;
+	}
 
 }
